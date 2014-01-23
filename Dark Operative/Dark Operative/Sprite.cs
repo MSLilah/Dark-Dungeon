46f6099c32494afdf105f853785f096eea2fe6c0
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-01-23 12:39:28 -0800

    Added a sprite class
    Added a new sprite class to the project, which represents an
    animated sprite on the screen. It currently contains variable
    declarations, variable properties, and a constructor.

---
+++ Dark Operative/Dark Operative/Sprite.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Dark_Operative
+{
+    class Sprite
+    {
+        #region Declarations
+        //The actual sprite itself
+        Texture2D spriteTexture;
+
+        //Controls the framerate for the sprite's animation
+        float frameRate = 0.02f;
+        float elapsedTime = 0.0f;
+
+        //Information on the spritesheet
+        int spriteSheetXOffset= 0;
+        int spriteSheetYOffset = 0;
+        int spriteWidth = 32;
+        int spriteHeight = 32;
+
+        //Animation information
+        int numberOfFrames = 1;
+        int currentFrame = 0;
+
+        //Position information
+        int xPosition = 0;
+        int yPosition = 0;
+
+        bool animating = true;
+
+        Color tinting = Color.White;
+        #endregion
+
+        public int X
+        {
+            set { xPosition = value; }
+            get { return xPosition; }
+        }
+
+        public int Y
+        {
+            get { return yPosition; }
+            set { yPosition = value; }
+        }
+
+        public int CurrFrame {
+            get { return currentFrame }
+            set { currentFrame = (int)MathHelper.Clamp(value, 0, numberOfFrames); }
+        }
+
+        public float FrameLength
+        {
+            get { return frameRate; }
+            set { frameRate = (float)Math.Max(value, 0f); }
+        }
+
+        public bool IsAnimating
+        {
+            get { return animating; }
+            set { animating = value; }
+        }
+
+        public Color Tint
+        {
+            get { return tinting; }
+            set { tinting = value; }
+        }
+
+        public Sprite(Texture2D spriteToSet,
+                        int xOffset,
+                        int yOffset,
+                        int width,
+                        int height,
+                        int numFrames)
+        {
+            spriteTexture = spriteToSet;
+            spriteSheetXOffset = xOffset;
+            spriteSheetYOffset = yOffset;
+            spriteWidth = width;
+            spriteHeight = height;
+            int numberOfFrames = numFrames;
+        }
+
+
+    }
+}
