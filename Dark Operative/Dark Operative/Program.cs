af27e908fe5d1a1d5124411199ebcbc8a64fecaf
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-01-23 11:53:49 -0800

    Initial commit of Visual Studio 2010 project
    This is the initial commit of the newly created Visual Studio 2010
    project for Dark Dungeon/Dark Operative (Working titles). It
    contains the initial folders and code generated when the project
    was created.

---
+++ Dark Operative/Dark Operative/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Dark_Operative
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
